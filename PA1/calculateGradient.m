be603f71e18883ac940a34850102ddb16d24b968
Akshit Kumar <akshitkumar100@gmail.com>
2017-08-22 08:59:02 +0530

    Add helper functions and complete back-prop code

--- PA1/calculateGradient.m
+++ PA1/calculateGradient.m
@@ -1,24 +1,53 @@
-function [grad_1,grad_2,grad_3, grad_4] = calculateGradient(X,y,weight_matrix_1,weight_matrix_2,weight_matrix_3,weight_matrix_4)
-    %% Forward Propogation
-    a1 = [X ; ones(1,size(X,2))]; % Add a bais term to all the data points
-    z2 = weight_matrix_1 * a1;
-    a2 = [sigmoid(z2) ; ones(1,size(z2,2))];
-    z3 = weight_matrix_2 * a2;
-    a3 = [sigmoid(z3) ; ones(1,size(z3,2))];
-    z4 = weight_matrix_3 * a3;
-    a4 = [sigmoid(z4) ; ones(1,size(z4,2))];
-    z5 = weight_matrix_4 * a4;
-    a5 = softmax(z5);
+function [L,grad_1,grad_2,grad_3,grad_4] = calculateGradient(X,y,num_labels,weight_matrix_1,weight_matrix_2,weight_matrix_3,weight_matrix_4)
+    %% Number of data points
+    m = size(X,1);
 
+    %% Convert y into one hot encoding
+    p = eye(num_labels);
+    Y = p(y,:);
+    
+    %% Forward Propogation
+    a1 = [ones(size(X,1),1) X]; % Add a bias term 
+    z2 = a1 * weight_matrix_1' ; 
+    a2 = [ones(size(z2,1),1) sigmoid(z2)];
+    z3 = a2 * weight_matrix_2';
+    a3 = [ones(size(z3,1),1) sigmoid(z3)];
+    z4 = a3 * weight_matrix_3';
+    a4 = [ones(size(z4,1),1) sigmoid(z4)];
+    z5 = a4 * weight_matrix_4';
+    a5 = softmax(z5')';
+
+    %% Calculate the loss function
+    L = -(sum(sum(Y.*log(a5))));
+    L = L/m;
+    reg = (lambda/(2*m)) * (sum(sum(weight_matrix_1(:,2:end).^2)) + sum(sum(weight_matrix_2(:,2:end).^2)) + sum(sum(weight_matrix_3(:,2:end).^2)) + ... 
+          sum(sum(weight_matrix_4(:,2:end).^2)));
+    L = L + reg;
+    
     %% Backward Propogation
+    %%% Calculating the deltas
     d5 = a5 - Y;
-    d4 = (weight_matrix_4' * d5).* [sigmoidGradient(z4) ; ones(1,size(z4,2))];
-    d3 = (weight_matrix_3' * d4) .* [sigmoidGradient(z3) ; ones(1,size(z3,2))];
-    d2 = (weight_matrix_2' * d3) .* [sigmoidGradient(z2) ; ones(1,size(z2,2))];
+    d4 = (d5 * weight_matrix_4) .* [ones(size(z4,1),1) sigmoidGradient(z4)];
+    d3 = (d4 * weight_matrix_3) .* [ones(size(z3,1),1) sigmoidGradient(z3)];
+    d2 = (d3 * weight_matrix_2) .* [ones(size(z2,1),1) sigmoidGradient(z2)];
+    
+    %%% Calculating the Deltas
+    D1 = d2(:,2:end)' * a1;
+    D2 = d3(:,2:end)' * a2;
+    D3 = d4(:,2:end)' * a3;
+    D4 = d5(:,2:end)' * a4;
     
     %% Calculate the Gradient
-    grad1 = 
-
+    grad_1 = D1/m;
+    grad_2 = D2/m;
+    grad_3 = D3/m;
+    grad_4 = D4/m;
+    
+    grad_1(:,2:end) = grad_1(:,2:end) + (lambda/m) * (weight_matrix_1(:,2:end));
+    grad_2(:,2:end) = grad_2(:,2:end) + (lambda/m) * (weight_matrix_2(:,2:end));
+    grad_3(:,2:end) = grad_3(:,2:end) + (lambda/m) * (weight_matrix_3(:,2:end));
+    grad_4(:,2:end) = grad_4(:,2:end) + (lambda/m) * (weight_matrix_4(:,2:end));
+    
 
 end
 
--- PA1/main.m
+++ PA1/main.m
@@ -22,15 +22,13 @@ output_layer_size = 10;
 weight_matrix_1 = randWeightInit(input_layer_size, hidden_layer_1_size);
 weight_matrix_2 = randWeightInit(hidden_layer_1_size,hidden_layer_2_size);
 weight_matrix_3 = randWeightInit(hidden_layer_2_size,hidden_layer_3_size);
-weight_matrix_4 = randWeightInit(hidden_layer_3_size,output_layer);
+weight_matrix_4 = randWeightInit(hidden_layer_3_size,output_layer_size);
 
-%% Training the neural network
+%% Training the neural network   
 for i = 1:10000
     r = randi([1 size(train_images,2)],1,64);
-    X = train_images(:,r);
-    y = train_labels(:,r);
-    [weight_grad_1 , weight_grad_2 , weight_grad_3 , weight_grad_4] = calculateGradient(X,y,weight_matrix_1,weight_matrix_2, weight_matrix_3, weight_matrix_4);
-    
-    
+    X = train_images(:,r);dient
+    y = train_labels(r,:);
+    [loss,weight_grad_1 , weight_grad_2 , weight_grad_3 , weight_grad_4] = calculateGradient(X',y,output_layer_size,weight_matrix_1,weight_matrix_2, weight_matrix_3, weight_matrix_4); 
     
     
