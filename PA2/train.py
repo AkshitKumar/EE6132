2adccf307d2e70b3e81034f30406675410afc67d
AkshitKumar <akshitkumar100@gmail.com>
2017-09-10 20:54:00 +0530

    Finish the first part of the assignment

--- PA2/train.py
+++ PA2/train.py
@@ -15,6 +15,10 @@ model2_validation_loss = list()
 model3_training_loss = list()
 model3_validation_loss = list()
 
+fig1 = plt.figure(1, (12.,12.))
+fig2 = plt.figure(2, (12.,12.))
+fig3 = plt.figure(3, (12.,12.))
+
 from model1 import *
 
 with tf.Session() as sess:
@@ -30,24 +34,21 @@ with tf.Session() as sess:
 		train_step.run(feed_dict={x:batch[0], y_:batch[1]})
 	print('Test Accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
-image_list = mnist.test.images[0:19]
-image_list_labels = mnist.test.labels[0:19]
-fig = plt.figure(1, (12.,12.))
-grid = ImageGrid(fig, 111, 
+	image_list = mnist.test.images[0:20]
+	image_list_labels = mnist.test.labels[0:20]
+	
+	grid = ImageGrid(fig1, 111, 
 				nrows_ncols=(4,5),
 				axes_pad = 0.5)
-prob = y.eval(feed_dict={x:image_list})
-pred_list = np.zeros(len(image_list)).astype(int)
+	prob = y.eval(feed_dict={x:image_list})
+	pred_list = np.zeros(len(image_list)).astype(int)
 
-for i in range(len(prob)):
-	pred_list[i] = np.argmax(prob[i])
-	image = image_list[i].reshape(28,28)
-	grid[i].imshow(image)
-	grid[i].set_title('True Label : {0} \n Predicted Label : {0}').format(image_list_labels[i].argmax,pred_list[i])
+	for i in range(len(prob)):
+		pred_list[i] = np.argmax(prob[i])
+		image = image_list[i].reshape(28,28)
+		grid[i].imshow(image)
+		grid[i].set_title('True:{0} \n Predicted:{1}'.format(np.argmax(image_list_labels[i]),pred_list[i]))
 
-plt.show()
-
-'''
 
 from model2 import *
 
@@ -63,6 +64,21 @@ with tf.Session() as sess:
 			print ('Model 2 Step : %d Validation Loss : %g') % (i,valid_loss)
 		train_step.run(feed_dict={x:batch[0], y_:batch[1]})
 	print('Test Accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+	
+	image_list = mnist.test.images[0:20]
+	image_list_labels = mnist.test.labels[0:20]
+	
+	grid = ImageGrid(fig2, 111, 
+				nrows_ncols=(4,5),
+				axes_pad = 0.5)
+	prob = y.eval(feed_dict={x:image_list})
+	pred_list = np.zeros(len(image_list)).astype(int)
+
+	for i in range(len(prob)):
+		pred_list[i] = np.argmax(prob[i])
+		image = image_list[i].reshape(28,28)
+		grid[i].imshow(image)
+		grid[i].set_title('True:{0} \n Predicted:{1}'.format(np.argmax(image_list_labels[i]),pred_list[i]))
 
 from model3 import *
 
@@ -78,9 +94,23 @@ with tf.Session() as sess:
 			print ('Model 3 Step : %d Validation Loss : %g') % (i,valid_loss)
 		train_step.run(feed_dict={x:batch[0], y_:batch[1]})
 	print('Test Accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+	image_list = mnist.test.images[0:20]
+	image_list_labels = mnist.test.labels[0:20]
+	
+	grid = ImageGrid(fig3, 111, 
+				nrows_ncols=(4,5),
+				axes_pad = 0.5)
+	prob = y.eval(feed_dict={x:image_list})
+	pred_list = np.zeros(len(image_list)).astype(int)
+
+	for i in range(len(prob)):
+		pred_list[i] = np.argmax(prob[i])
+		image = image_list[i].reshape(28,28)
+		grid[i].imshow(image)
+		grid[i].set_title('True:{0} \n Predicted:{1}'.format(np.argmax(image_list_labels[i]),pred_list[i]))
 
 epochs = np.arange(1,len(model1_training_loss)+1)
-plt.figure(1)
+plt.figure(4)
 plt.plot(epochs,model1_training_loss,epochs,model2_training_loss,epochs,model3_training_loss)
 plt.xlabel('Training Epochs')
 plt.ylabel('Cross Entropy Training Loss')
@@ -88,11 +118,10 @@ plt.legend(["Model 1","Model 2","Model 3"])
 plt.title('Plot of Training Loss vs Epoch for different models')
 
 epochs = np.arange(1,len(model1_validation_loss)+1)
-plt.figure(2)
+plt.figure(5)
 plt.plot(epochs,model1_validation_loss,epochs,model2_validation_loss,epochs,model3_validation_loss)
 plt.xlabel('Training Epochs')
 plt.ylabel('Cross Entropy Validation Loss')
 plt.legend(["Model 1","Model 2","Model 3"])
 plt.title('Plot of Validation Loss vs Epoch for different models')
 plt.show()
-'''
