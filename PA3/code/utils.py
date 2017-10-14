3cd6662a1597fdec4b8aad1bf1ac1f562f00520f
AkshitKumar <akshitkumar100@gmail.com>
2017-10-15 01:06:11 +0530

    Finish vanilla RNN code

--- PA3/code/utils.py
+++
@@ -1,4 +0,0 @@
-from tensorflow.examples.tutorials.mnist import input_data
-
-def loadData(validationSize = None):
-	return input_data.read_data_sets('MNIST_data',one_hot = True, validation_size = validationSize)
