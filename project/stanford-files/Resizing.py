be7248a095bbff2ebce69caafff66b883fa4ff33
AkshitKumar <akshitkumar100@gmail.com>
2017-10-21 14:56:33 +0530

    update resize file

--- project/stanford-files/Resizing.py
+++ project/stanford-files/Resizing.py
@@ -1,13 +1,9 @@
-
-# coding: utf-8
-
-# In[1]:
-
 from PIL import Image
 import datautils
 import numpy as np
 import matplotlib.pyplot as plt
 
+'''
 get_ipython().magic(u'matplotlib inline')
 plt.rcParams['figure.figsize'] = (5.0, 5.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
@@ -15,18 +11,12 @@ plt.rcParams['image.cmap'] = 'gray'
 
 get_ipython().magic(u'load_ext autoreload')
 get_ipython().magic(u'autoreload 2')
-
-
-# In[2]:
+'''
 
 res = 128
 datautils.resize_images(res=res)
+'''
 im_data = datautils.load_image("data/png{}/laptop/9555.png".format(res))
 plt.imshow(im_data.reshape(res, res))
 plt.show()
-
-
-# In[ ]:
-
-
-
+'''
