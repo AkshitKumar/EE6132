8735e6ec605e60f350813ac20cba7bf317e37f35
AkshitKumar <akshitkumar100@gmail.com>
2017-10-21 14:40:35 +0530

    Add project temp files

---
+++ project/stanford-files/ResNet-Dropout.py
@@ -0,0 +1,210 @@
+
+# coding: utf-8
+
+# In[1]:
+
+from __future__ import division
+
+import datautils
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import tensorflow as tf
+import math
+import models
+
+get_ipython().magic(u'matplotlib inline')
+plt.rcParams['figure.figsize'] = (5.0, 5.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+get_ipython().magic(u'load_ext autoreload')
+get_ipython().magic(u'autoreload 2')
+
+
+# In[ ]:
+
+# Read in the data
+num_classes = 250 #250
+res = 128
+
+tic = time.clock()
+X_train, y_train, X_val, y_val, X_test, y_test, labels = datautils.get_data(num_classes=num_classes, res=128, flip=True)
+toc = time.clock()
+print ("Read {} images in {:5} seconds".format(X_train.shape[0] + X_val.shape[0] + X_test.shape[0], toc - tic))
+print ("X_train: ", X_train.shape)
+print ("y_train: ", y_train.shape)
+print ("X_val: ", X_val.shape)
+print ("y_val: ", y_val.shape)
+print ("X_test: ", X_test.shape)
+print ("y_test: ", y_test.shape)
+
+
+# In[9]:
+
+# Plot a random image
+sample = np.random.randint(X_train.shape[0])
+sample = 0
+plt.imshow(X_train[sample].reshape(128, 128))
+plt.show()
+plt.imshow(X_train[sample + (1 if sample % 2 == 0 else -1)].reshape(128, 128))
+plt.show()
+print (labels[y_train[sample]])
+
+
+# In[7]:
+
+tf.reset_default_graph()
+
+X = tf.placeholder(tf.float32, [None, res, res, 1])
+y = tf.placeholder(tf.int64, [None])
+is_training = tf.placeholder(tf.bool)
+lr = tf.placeholder(tf.float32)
+reg = tf.placeholder(tf.float32)
+
+y_out = models.resnet_dropout(X, y, layer_depth=3, num_classes=num_classes, is_training=is_training, reg=reg)
+print (y_out.shape)
+mean_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf.one_hot(y, num_classes), logits=y_out))
+optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+
+# batch normalization in tensorflow requires this extra dependency
+extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+with tf.control_dependencies(extra_update_ops):
+    train_step = optimizer.minimize(mean_loss)
+reg_val = 1e-2
+learning_rate = 1e-3
+
+
+# In[8]:
+
+def run_model(session, predict, loss_val, Xd, yd,
+              epochs=1, batch_size=64, print_every=100,
+              training=None, plot_losses=False):
+    # have tensorflow compute accuracy
+    correct_prediction = tf.equal(tf.argmax(predict,1), y)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    
+    # shuffle indicies
+    train_indicies = np.arange(Xd.shape[0])
+    np.random.shuffle(train_indicies)
+
+    training_now = training is not None
+    
+    # setting up variables we want to compute (and optimizing)
+    # if we have a training function, add that to things we compute
+    variables = [mean_loss,correct_prediction,accuracy]
+    if training_now:
+        variables[-1] = training
+    
+    # counter 
+    iter_cnt = 0
+    for e in range(epochs):
+                # keep track of losses and accuracy
+        correct = 0
+        losses = []
+        # make sure we iterate over the dataset once
+        for i in range(int(math.ceil(Xd.shape[0]/batch_size))):
+                        # generate indicies for the batch
+            start_idx = (i*batch_size)%X_train.shape[0]
+            idx = train_indicies[start_idx:start_idx+batch_size]
+            
+            # create a feed dictionary for this batch
+            feed_dict = {X: Xd[idx,:], y: yd[idx], is_training:training_now,
+                         lr : learning_rate, reg: reg_val}
+                        
+                        
+            # get batch size
+            actual_batch_size = yd[i:i+batch_size].shape[0]
+            
+            # have tensorflow compute loss and correct predictions
+            # and (if given) perform a training step
+            loss, corr, _ = session.run(variables,feed_dict=feed_dict)
+            
+            # aggregate performance stats
+            losses.append(loss*actual_batch_size)
+            correct += np.sum(corr)
+            
+            # print every now and then
+            if training_now and (iter_cnt % print_every) == 0:
+                print("Iteration {0}: with minibatch training loss = {1:.3g} and accuracy of {2:.2g}"                                                              .format(iter_cnt,loss,np.sum(corr)/actual_batch_size))
+            iter_cnt += 1
+        total_correct = correct/Xd.shape[0]
+        total_loss = np.sum(losses)/Xd.shape[0]
+        print("Epoch {2}, Overall loss = {0:.3g} and accuracy of {1:.3g}"                              .format(total_loss,total_correct,e+1))
+        if plot_losses:
+            plt.plot(losses)
+            plt.grid(True)
+            plt.title('Epoch {} Loss'.format(e+1))
+            plt.xlabel('minibatch number')
+            plt.ylabel('minibatch loss')
+            plt.show()
+    return total_loss,total_correct
+
+
+# In[9]:
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+train_losses = []
+train_acc = []
+val_losses = []
+val_acc = []
+
+
+# In[ ]:
+
+epochs = 1
+reg_val = 1e-1
+learning_rate = 1e-3
+for i in range(epochs):
+    loss, acc = run_model(session=sess,
+                                 predict=y_out,
+                                 loss_val=mean_loss,
+                                 Xd=X_train,
+                                 yd=y_train,
+                                 epochs=1,
+                                 batch_size=256,
+                                 print_every=5,
+                                 training=train_step,
+                                 plot_losses=False)
+    train_losses.append(loss)
+    train_acc.append(acc)
+    loss, acc = run_model(session=sess,
+                                 predict=y_out,
+                                 loss_val=mean_loss,
+                                 Xd=X_val,
+                                 yd=y_val,
+                                 epochs=1,
+                                 batch_size=64,
+                                 print_every=10000,
+                                 training=None,
+                                 plot_losses=False)
+    val_losses.append(loss)
+    val_acc.append(acc)
+
+
+# In[8]:
+
+plt.plot(train_losses, label='Train')
+plt.plot(val_losses, label='Val')
+plt.grid(True)
+plt.title('Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.plot(train_acc, label='Train')
+plt.plot(val_acc, label='Val')
+plt.grid(True)
+plt.title('Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+
+# In[ ]:
+
+
+
---
+++ project/stanford-files/datautils2.py
@@ -0,0 +1,134 @@
+import os
+import numpy as np
+import PIL
+from PIL import Image
+from scipy.misc import imread, imresize, toimage
+import scipy.ndimage
+
+# Functions which need the PIL
+
+def get_data(num_classes=250, res=128, flip=True, color_invert=True, center=False,rotate_3=True):
+    """
+    Generates the datasets with 128 (or 64) training examples, 8 validation examples,
+    and 8 testing examples per class.
+    
+    Args:
+        num_classes: the number of classes to load (for smaller datasets)
+        res: the resolution of the output arrays (N x res x res)
+        flip: whether or not to generate additional training examples by horizontally
+              flipping the provided images
+        color_invert: whether or not to invert B&W values
+    """
+    root_dir = "data/png{}/".format("" if res is None else res)
+    #root_dir = "data/png{}/".format("" )
+
+    num_train = 96 if flip else 48
+    num_train =3*(num_train) if rotate_3 else num_train
+    num_val = 16
+    num_test = 16
+    
+    labels = []
+    
+    X_train = np.zeros((num_classes * num_train, res, res, 1), dtype=np.float32)
+    y_train = np.repeat(np.arange(num_classes), num_train)
+    
+    X_val = np.zeros((num_classes * num_val, res, res, 1), dtype=np.float32)
+    y_val = np.repeat(np.arange(num_classes), num_val)
+    
+    X_test = np.zeros((num_classes * num_test, res, res, 1), dtype=np.float32)
+    y_test = np.repeat(np.arange(num_classes), num_test)
+    
+    classes = 0
+    train_index = 0
+    val_index = 0
+    test_index = 0
+    
+    for node in sorted(os.listdir(root_dir)):
+        if os.path.isfile(root_dir + node):
+            continue
+        
+        labels.append(node)
+        label_path = root_dir + node + "/"
+        
+        num_images = 0
+        for im_file in sorted(os.listdir(label_path)):
+            im_data = load_image(label_path + im_file).reshape(res, res, 1)
+            
+            if color_invert:
+                im_data = -1 * im_data + 255
+            
+            if num_images < num_train:
+                X_train[train_index] = im_data
+                train_index += 1
+                if flip:
+                    flipped=np.flip(im_data,axis=1)
+                    X_train[train_index] = flipped
+                    train_index += 1
+                    num_images += 1
+                if rotate_3:
+               	    X_train[train_index] = scipy.ndimage.interpolation.rotate(im_data,10,cval=0,reshape=False)
+                    train_index += 1
+                    num_images += 1
+               	    X_train[train_index] = scipy.ndimage.interpolation.rotate(im_data,-10,cval=0,reshape=False)
+                    train_index += 1
+                    num_images += 1
+                    if flip:					
+                        X_train[train_index] = scipy.ndimage.interpolation.rotate(flipped,10,cval=0,reshape=False)
+                        train_index += 1
+                        num_images += 1
+               	        X_train[train_index] = scipy.ndimage.interpolation.rotate(flipped,-10,cval=0,reshape=False)
+                        train_index += 1
+                        num_images += 1				
+            elif num_images < num_train + num_val:
+                X_val[val_index] = im_data
+                val_index += 1
+            else:
+                X_test[test_index] = im_data
+                test_index += 1
+                
+            num_images += 1
+                
+        classes += 1
+        if classes == num_classes:
+            break
+
+    if center:
+        X_train -= np.mean(X_train, axis=0)
+        X_val -= np.mean(X_val, axis=0)
+        X_test -= np.mean(X_test, axis=0)
+    return X_train, y_train, X_val, y_val, X_test, y_test, labels
+
+def load_image(path):
+    im_data = imread(path, mode='L')
+    return im_data
+
+def resize_images(res=128):
+    root_dir = "data/png/"
+    new_dir = "data/png{}/".format(res)
+    try:
+        os.mkdir(new_dir, 755)
+    except:
+        pass
+    
+    for node in sorted(os.listdir(root_dir)):
+        if os.path.isfile(root_dir + node):
+            continue
+            
+        print ("Resizing {}".format(node))
+        
+        label_path = root_dir + node + "/"
+        new_path = new_dir + node + "/"
+        
+        try:
+            os.mkdir(new_path, 755)
+        except:
+            pass
+        
+        for im_file in os.listdir(label_path):
+            im_path = label_path + im_file
+            new_im_path = new_path + im_file
+            
+            img = Image.open(im_path)
+            img = img.resize((res, res), PIL.Image.BILINEAR)
+            img.save(new_im_path)
+            img.close()
