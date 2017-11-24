cd7e61413a6c712aff0135aac48742b57ef920b1
AkshitKumar <akshitkumar100@gmail.com>
2017-11-24 11:50:31 +0530

    change file download

--- project/sketch-xnor/dataset/get_dataset.sh
+++ project/sketch-xnor/dataset/get_dataset.sh
@@ -1,4 +1,5 @@
 # Get Sketch Dataset
-curl http://cybertron.cg.tu-berlin.de/eitz/projects/classifysketch/sketches_svg.zip -o sketches_svg.zip
-unzip sketches_svg.zip
-rm sketches_svg.zip
+#curl http://cybertron.cg.tu-berlin.de/eitz/projects/classifysketch/sketches_svg.zip -o sketches_svg.zip
+#unzip sketches_svg.zip
+#rm sketches_svg.zip
+wget -O dataset_without_order_info_256.mat  https://www.dropbox.com/s/cfq9r3mchh33r25/dataset_without_order_info_256.mat?dl=1
