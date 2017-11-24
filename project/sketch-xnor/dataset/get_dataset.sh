3dbb625eb6e8c0e391389cff868c9b8c01ae36ec
AkshitKumar <akshitkumar100@gmail.com>
2017-11-24 22:21:27 +0530

    change download script

--- project/sketch-xnor/dataset/get_dataset.sh
+++ project/sketch-xnor/dataset/get_dataset.sh
@@ -4,5 +4,5 @@
 #rm sketches_svg.zip
 wget -O dataset_without_order_info_256.mat  https://www.dropbox.com/s/cfq9r3mchh33r25/dataset_without_order_info_256.mat?dl=1
 wget -O dataset_with_order_info_256.mat https://www.dropbox.com/s/kuwgd793747dgbz/dataset_with_order_info_256.mat?dl=1
-wget -O model_with_order_info_256.mat https://www.dropbox.com/s/6tnzmjof66fs5a9/model_with_order_info_256.mat?dl=1
-wget -O model_without_order_info_256.mat  https://www.dropbox.com/s/70300ohvtzb30b7/model_without_order_info_256.mat?dl=1
+#wget -O model_with_order_info_256.mat https://www.dropbox.com/s/6tnzmjof66fs5a9/model_with_order_info_256.mat?dl=1
+#wget -O model_without_order_info_256.mat  https://www.dropbox.com/s/70300ohvtzb30b7/model_without_order_info_256.mat?dl=1
